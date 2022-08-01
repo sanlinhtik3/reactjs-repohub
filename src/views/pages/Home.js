a39380a04301af197205f2bcfa2a5f4e8732056f
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-08-01 22:43:48 +0630

    Helmet

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -3,12 +3,16 @@ import Service_1 from "../../assets/img/our_service_1.png";
 import Service_2 from "../../assets/img/our_service_2.png";
 import Service_3 from "../../assets/img/our_service_3.png";
 import Service_4 from "../../assets/img/our_service_4.png";
-import {NavLink} from "react-router-dom";
+import Logo from '../../repohub_logo.png';
+import Img from '../../assets/img/img.png';
+import {Link, NavLink} from "react-router-dom";
 import {useEffect} from "react";
 import ScrollReveal from "scrollreveal";
+import {Helmet} from "react-helmet";
 
 
 const Home = (props) => {
+
 
     const Works = [
         {id: 1, src: Home_Img_1, subtitle: "Sportbooks", desc: "Rewind"},
@@ -26,8 +30,8 @@ const Home = (props) => {
         {id: 4, src: Service_4, title: "Research Analysis",},
     ]
 
-    useEffect(()=>{
-        ScrollReveal().reveal('.headline',{
+    useEffect(() => {
+        ScrollReveal().reveal('.headline', {
             origin: 'bottom',
             distance: '40px',
             duration: 1200,
@@ -36,10 +40,28 @@ const Home = (props) => {
             scale: 0.9,
             viewFactor: 0.1,
         });
-    },[])
+    }, [])
 
     return (
         <div>
+
+            <Helmet>
+                <title>RepoHub</title>
+                <meta name="title" content="RepoHub" />
+                <meta name="description" content="ကျောင်းသား/သူ များအတွက် Project" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="repohub.co" />
+                <meta property="og:title" content="RepoHub" />
+                <meta property="og:description" content="ကျောင်းသား/သူ များအတွက် Project" />
+                <meta property="og:image" content={Img} />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="repohub.co" />
+                <meta property="twitter:title" content="RepoHub" />
+                <meta property="twitter:description" content="ကျောင်းသား/သူ များအတွက် Project" />
+                <meta property="twitter:image" content={Img} />
+            </Helmet>
 
             {/*Home Section*/}
 
@@ -51,10 +73,11 @@ const Home = (props) => {
                                 <div className="col-12">
                                     <div className="my-5 py-5">
                                         <h3 className="fw-light headline">Digital Agency</h3>
-                                        <h3 className="mb-5 headline">
+                                        <h3 className="mb-5 Walone-Bold headline">
                                             We developer complex interface systems for Web, Mobile & Ar
                                         </h3>
-                                        <img src={Home_Img_1} className="img-fluid headline pdf-rounded-xl" alt="Home img"/>
+                                        <img src={Home_Img_1} className="img-fluid headline pdf-rounded-xl"
+                                             alt="Home img"/>
                                     </div>
                                 </div>
                             </div>
@@ -62,7 +85,6 @@ const Home = (props) => {
                     </div>
                 </div>
             </div>
-
 
 
             {/*Work Section*/}
@@ -78,19 +100,21 @@ const Home = (props) => {
                                             <h3 className="text-center mb-5 headline">Our Work Select Projects</h3>
 
                                             <div className="row row-cols-1 row-cols-md-2 g-5">
-                                                {Works.map( (work) => {
+                                                {Works.map((work) => {
                                                     return (
                                                         <div key={work.id} className="col headline">
-                                                            <div className="card rounded pdf-rounded-xl overflow-hidden border-0 shadow-xl shadow-blue-50">
-                                                                <img src={work.src} className="img-fluid" alt="Home img"/>
+                                                            <div
+                                                                className="card rounded pdf-rounded-xl overflow-hidden border-0 shadow-xl shadow-blue-50">
+                                                                <img src={work.src} className="img-fluid"
+                                                                     alt="Home img"/>
                                                                 <div className="card-body">
-                                                                    <h5 className="card-title">{work.subtitle}</h5>
-                                                                    <p className="card-text">{work.desc}</p>
+                                                                    <h5 className="card-title Walone-Bold">{work.subtitle}</h5>
+                                                                    <p className="card-text text-secondary">{work.desc}</p>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     )
-                                                } )}
+                                                })}
 
                                             </div>
 
@@ -112,11 +136,12 @@ const Home = (props) => {
                             <section className="container">
                                 <div className="row">
                                     <div className="col-lg-6">
-                                        <h4 className="headline">Our Services</h4>
+                                        <h4 className="Walone-Bold headline">Our Services</h4>
                                     </div>
                                     <div className="col-lg-6">
-                                        <p className="headline">
-                                            As a strategic partner, we create a working product with a thoughtful and large-scale architecture. We launch, support and development.
+                                        <p className="text-secondary headline">
+                                            As a strategic partner, we create a working product with a thoughtful and
+                                            large-scale architecture. We launch, support and development.
                                         </p>
                                     </div>
                                 </div>
@@ -124,7 +149,8 @@ const Home = (props) => {
                                     {Services.map(service => {
                                         return (
                                             <div key={service.id} className="col headline">
-                                                <div className="card text-center py-3 py-lg-5 border-0 shadow-xl shadow-blue-50">
+                                                <div
+                                                    className="card text-center py-3 py-lg-5 border-0 shadow-xl shadow-blue-50">
                                                     <div className="mb-1 mb-lg-3">
                                                         <img src={service.src} className="w-50" alt="Home img"/>
                                                     </div>
@@ -163,11 +189,13 @@ const Home = (props) => {
                                 <div className="row">
                                     <div className="col">
                                         <div className="my-lg-5 py-lg-5">
-                                            <div className="text-center py-5 my-5 headline">
-                                                <p>next</p>
-                                                <h4>Our Story</h4>
-                                                <i className="fa-solid fa-arrow-right-long"></i>
-                                            </div>
+                                            <Link to="/story" className="text-decoration-none text-secondary">
+                                                <div className="text-center py-5 my-5 headline">
+                                                    <p className="text-uppercase">next</p>
+                                                    <h4 className="text-black">Our Story</h4>
+                                                    <i className="fa-solid fa-arrow-right-long"></i>
+                                                </div>
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
@@ -185,12 +213,18 @@ const Home = (props) => {
                                 <div className="row">
                                     <div className="col col-lg-auto col-md-10 headline">
                                         <div className="small mb-4 mb-lg-0">
-                                            <NavLink to="/" className="text-decoration-none text-secondary me-4">Home</NavLink>
-                                            <NavLink to="/story" className="text-decoration-none text-secondary me-4">Story</NavLink>
-                                            <NavLink to="/services" className="text-decoration-none text-secondary me-4">Works</NavLink>
+                                            <NavLink to="/"
+                                                     className="text-decoration-none text-secondary me-4">Home</NavLink>
+                                            <NavLink to="/story"
+                                                     className="text-decoration-none text-secondary me-4">Story</NavLink>
+                                            <NavLink to="/services"
+                                                     className="text-decoration-none text-secondary me-4">Works</NavLink>
 
-                                            <NavLink to="/services" className="text-decoration-none text-secondary d-lg-none">Careers</NavLink>
-                                            <NavLink to="/contact" className="text-decoration-none text-secondary d-lg-none ms-4">Contact Us</NavLink>
+                                            <NavLink to="/services"
+                                                     className="text-decoration-none text-secondary d-lg-none">Careers</NavLink>
+                                            <NavLink to="/contact"
+                                                     className="text-decoration-none text-secondary d-lg-none ms-4">Contact
+                                                Us</NavLink>
                                         </div>
                                     </div>
                                     <div className="col headline">
@@ -200,8 +234,10 @@ const Home = (props) => {
                                     </div>
                                     <div className="col d-none d-lg-block headline">
                                         <div className="text-end small">
-                                            <NavLink to="/services" className="text-decoration-none text-secondary">Careers</NavLink>
-                                            <NavLink to="/contact" className="text-decoration-none text-secondary ms-4">Contact Us</NavLink>
+                                            <NavLink to="/services"
+                                                     className="text-decoration-none text-secondary">Careers</NavLink>
+                                            <NavLink to="/contact" className="text-decoration-none text-secondary ms-4">Contact
+                                                Us</NavLink>
                                         </div>
                                     </div>
                                 </div>
@@ -212,7 +248,7 @@ const Home = (props) => {
             </div>
 
         </div>
-    )
+)
 }
 
 export default Home;
